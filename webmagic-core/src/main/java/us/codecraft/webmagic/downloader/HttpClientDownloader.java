77b3736dfcb3343fe251e58e03e067d91800055b
Luc Esape <luc.esape@gmail.com>
2017-05-09 22:11:52 +0200

    Bug commit from the following repository code4craft/webmagic This bug commit is a reflect of source code from: http://github.com/code4craft/webmagic/commit/11c32669b23cc404be2ca2be0dd1dc112c420520.

---
+++ webmagic-core/src/main/java/us/codecraft/webmagic/downloader/HttpClientDownloader.java
@@ -0,0 +1,226 @@
+package us.codecraft.webmagic.downloader;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.annotation.ThreadSafe;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.proxy.Proxy;
+import us.codecraft.webmagic.selector.PlainText;
+import us.codecraft.webmagic.utils.CharsetUtils;
+import us.codecraft.webmagic.utils.HttpConstant;
+import us.codecraft.webmagic.utils.WMCollections;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.*;
+
+
+/**
+ * The http downloader based on HttpClient.
+ *
+ * @author code4crafter@gmail.com <br>
+ * @since 0.1.0
+ */
+@ThreadSafe
+public class HttpClientDownloader extends AbstractDownloader {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final Map<String, CloseableHttpClient> httpClients = new HashMap<String, CloseableHttpClient>();
+
+    private HttpClientGenerator httpClientGenerator = new HttpClientGenerator();
+
+    private CloseableHttpClient getHttpClient(Site site, Proxy proxy) {
+        if (site == null) {
+            return httpClientGenerator.getClient(null, proxy);
+        }
+        String domain = site.getDomain();
+        CloseableHttpClient httpClient = httpClients.get(domain);
+        if (httpClient == null) {
+            synchronized (this) {
+                httpClient = httpClients.get(domain);
+                if (httpClient == null) {
+                    httpClient = httpClientGenerator.getClient(site, proxy);
+                    httpClients.put(domain, httpClient);
+                }
+            }
+        }
+        return httpClient;
+    }
+
+    @Override
+    public Page download(Request request, Task task) {
+        Site site = null;
+        if (task != null) {
+            site = task.getSite();
+        }
+        Set<Integer> acceptStatCode;
+        String charset = null;
+        Map<String, String> headers = null;
+        if (site != null) {
+            acceptStatCode = site.getAcceptStatCode();
+            charset = site.getCharset();
+            headers = site.getHeaders();
+        } else {
+            acceptStatCode = WMCollections.newHashSet(200);
+        }
+        logger.info("downloading page {}", request.getUrl());
+        CloseableHttpResponse httpResponse = null;
+        int statusCode=0;
+        try {
+            HttpHost proxyHost = null;
+            Proxy proxy = null; //TODO
+            if (site.getHttpProxyPool() != null && site.getHttpProxyPool().isEnable()) {
+                proxy = site.getHttpProxyFromPool();
+                proxyHost = proxy.getHttpHost();
+            } else if(site.getHttpProxy()!= null){
+                proxyHost = site.getHttpProxy();
+            }
+            
+            HttpUriRequest httpUriRequest = getHttpUriRequest(request, site, headers, proxyHost);
+            httpResponse = getHttpClient(site, proxy).execute(httpUriRequest);
+            statusCode = httpResponse.getStatusLine().getStatusCode();
+            request.putExtra(Request.STATUS_CODE, statusCode);
+            if (statusAccept(acceptStatCode, statusCode)) {
+                Page page = handleResponse(request, charset, httpResponse, task);
+                onSuccess(request);
+                return page;
+            } else {
+                logger.warn("get page {} error, status code {} ",request.getUrl(),statusCode);
+                return null;
+            }
+        } catch (IOException e) {
+            logger.warn("download page {} error", request.getUrl(), e);
+            if (site.getCycleRetryTimes() > 0) {
+                return addToCycleRetry(request, site);
+            }
+            onError(request);
+            return null;
+        } finally {
+        	request.putExtra(Request.STATUS_CODE, statusCode);
+            if (site.getHttpProxyPool()!=null && site.getHttpProxyPool().isEnable()) {
+                site.returnHttpProxyToPool((HttpHost) request.getExtra(Request.PROXY), (Integer) request
+                        .getExtra(Request.STATUS_CODE));
+            }
+            try {
+                if (httpResponse != null) {
+                    //ensure the connection is released back to pool
+                    EntityUtils.consume(httpResponse.getEntity());
+                }
+            } catch (IOException e) {
+                logger.warn("close response fail", e);
+            }
+        }
+    }
+
+    @Override
+    public void setThread(int thread) {
+        httpClientGenerator.setPoolSize(thread);
+    }
+
+    protected boolean statusAccept(Set<Integer> acceptStatCode, int statusCode) {
+        return acceptStatCode.contains(statusCode);
+    }
+
+    protected HttpUriRequest getHttpUriRequest(Request request, Site site, Map<String, String> headers,HttpHost proxy) {
+        RequestBuilder requestBuilder = selectRequestMethod(request).setUri(request.getUrl());
+        if (headers != null) {
+            for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+                requestBuilder.addHeader(headerEntry.getKey(), headerEntry.getValue());
+            }
+        }
+        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom()
+                .setConnectionRequestTimeout(site.getTimeOut())
+                .setSocketTimeout(site.getTimeOut())
+                .setConnectTimeout(site.getTimeOut())
+                .setCookieSpec(CookieSpecs.BEST_MATCH);
+        if (proxy !=null) {
+			requestConfigBuilder.setProxy(proxy);
+			request.putExtra(Request.PROXY, proxy);
+		}
+        requestBuilder.setConfig(requestConfigBuilder.build());
+        return requestBuilder.build();
+    }
+
+    protected RequestBuilder selectRequestMethod(Request request) {
+        String method = request.getMethod();
+        if (method == null || method.equalsIgnoreCase(HttpConstant.Method.GET)) {
+            //default get
+            RequestBuilder requestBuilder=RequestBuilder.get();
+            if (request.getParams() != null) {
+                for (Map.Entry<String, String> entry : request.getParams().entrySet()) {
+                    requestBuilder.addParameter(entry.getKey(), entry.getValue());
+                }
+            }
+            return requestBuilder;
+        } else if (method.equalsIgnoreCase(HttpConstant.Method.POST)) {
+            RequestBuilder requestBuilder = RequestBuilder.post();
+            NameValuePair[] nameValuePair = (NameValuePair[]) request.getExtra("nameValuePair");
+            List<NameValuePair> allNameValuePair=new ArrayList<NameValuePair>();
+            if (nameValuePair != null && nameValuePair.length > 0) {
+                allNameValuePair= Arrays.asList(nameValuePair);
+            }
+            if (request.getParams() != null) {
+                for (String key : request.getParams().keySet()) {
+                    allNameValuePair.add(new BasicNameValuePair(key, request.getParams().get(key)));
+                }
+            }
+            requestBuilder.setEntity(new UrlEncodedFormEntity(allNameValuePair, Charset.forName("utf8")));
+            return requestBuilder;
+        } else if (method.equalsIgnoreCase(HttpConstant.Method.HEAD)) {
+            return RequestBuilder.head();
+        } else if (method.equalsIgnoreCase(HttpConstant.Method.PUT)) {
+            return RequestBuilder.put();
+        } else if (method.equalsIgnoreCase(HttpConstant.Method.DELETE)) {
+            return RequestBuilder.delete();
+        } else if (method.equalsIgnoreCase(HttpConstant.Method.TRACE)) {
+            return RequestBuilder.trace();
+        }
+        throw new IllegalArgumentException("Illegal HTTP Method " + method);
+    }
+
+    protected Page handleResponse(Request request, String charset, HttpResponse httpResponse, Task task) throws IOException {
+        String content = getContent(charset, httpResponse);
+        Page page = new Page();
+        page.setRawText(content);
+        page.setUrl(new PlainText(request.getUrl()));
+        page.setRequest(request);
+        page.setStatusCode(httpResponse.getStatusLine().getStatusCode());
+        return page;
+    }
+
+    protected String getContent(String charset, HttpResponse httpResponse) throws IOException {
+        if (charset == null) {
+            byte[] contentBytes = IOUtils.toByteArray(httpResponse.getEntity().getContent());
+            String htmlCharset = getHtmlCharset(httpResponse, contentBytes);
+            if (htmlCharset != null) {
+                return new String(contentBytes, htmlCharset);
+            } else {
+                logger.warn("Charset autodetect failed, use {} as charset. Please specify charset in Site.setCharset()", Charset.defaultCharset());
+                return new String(contentBytes);
+            }
+        } else {
+            return IOUtils.toString(httpResponse.getEntity().getContent(), charset);
+        }
+    }
+
+    protected String getHtmlCharset(HttpResponse httpResponse, byte[] contentBytes) throws IOException {
+        return CharsetUtils.detectCharset(httpResponse.getEntity().getContentType().getValue(), contentBytes);
+    }
+}
