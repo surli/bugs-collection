47823295e5e26265bbffb7a78d2c7c9e379bd750
Luc Esape <luc.esape@gmail.com>
2017-05-11 14:19:19 +0200

    Bug commit from the following repository tipsy/j2html This bug commit is a reflect of source code from: http://github.com/tipsy/j2html/commit/3d43eb159fbab280015668a8899c93e37c5e2ad1.

---
+++ src/main/java/j2html/tags/EmptyTag.java
@@ -0,0 +1,14 @@
+package j2html.tags;
+
+public class EmptyTag extends Tag<EmptyTag> {
+
+    public EmptyTag(String tagName) {
+        super(tagName);
+    }
+
+    @Override
+    public String render() {
+        return renderOpenTag();
+    }
+
+}
