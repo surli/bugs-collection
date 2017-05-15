ba999f92228ede6627f5210c627d548bf9a90805
Luc Esape <luc.esape@gmail.com>
2017-05-15 17:02:03 +0200

    Bug commit from the following repository languagetool-org/languagetool This bug commit is a reflect of source code from: http://github.com/languagetool-org/languagetool/commit/21ae1d5a5eabb73374e3ae7d3b91038f4fbcb299.

---
+++ languagetool-language-modules/ca/src/main/java/org/languagetool/language/ValencianCatalan.java
@@ -0,0 +1,58 @@
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2009 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+package org.languagetool.language;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ValencianCatalan extends Catalan {
+
+  @Override
+  public String getName() {
+    return "Catalan (Valencian)";
+  }
+
+  @Override
+  public String[] getCountries() {
+    return new String[]{"ES"};
+  }
+
+  @Override
+  public String getVariant() {
+    return "valencia";
+  }
+  
+  @Override
+  public List<String> getDefaultEnabledRulesForVariant() {
+    List<String> rules = Arrays.asList("EXIGEIX_VERBS_VALENCIANS",
+        "EXIGEIX_ACCENTUACIO_VALENCIANA", "EXIGEIX_POSSESSIUS_U",
+        "EXIGEIX_VERBS_EIX", "EXIGEIX_VERBS_ISC", "SERVIR_PER_TALLAR");
+    return Collections.unmodifiableList(rules);
+  }
+
+  @Override
+  public List<String> getDefaultDisabledRulesForVariant() {
+    List<String> rules = Arrays.asList("EXIGEIX_VERBS_CENTRAL",
+        "EXIGEIX_ACCENTUACIO_GENERAL", "EXIGEIX_POSSESSIUS_V",
+        "EVITA_PRONOMS_VALENCIANS", "EVITA_DEMOSTRATIUS_EIXE", "VOCABULARI_VALENCIA");
+    return Collections.unmodifiableList(rules);
+  }
+  
+}
