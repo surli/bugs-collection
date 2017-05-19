7f7834bb4bc3399137eaf3dd2b634854b4ba5414
Luc Esape <luc.esape@gmail.com>
2017-05-19 14:33:40 +0200

    Bug commit from the following repository rest-assured/rest-assured This bug commit is a reflect of source code from: http://github.com/rest-assured/rest-assured/commit/ea810fc8b7dd0c5e2afa2e582b4553dcef80986c.

---
+++ rest-assured/src/test/java/io/restassured/assertion/CookieMatcherMessagesTest.java
@@ -0,0 +1,66 @@
+package io.restassured.assertion;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author Maciej Gawinecki
+ */
+@SuppressWarnings("unchecked")
+public class CookieMatcherMessagesTest {
+
+    private String[] cookies = new String[]{"DEVICE_ID=123; Domain=.test.com; Expires=Thu, 12-Oct-2023 09:34:31 GMT; Path=/; Secure; HttpOnly;"};
+
+    @Test
+    public void shouldPrintValidErrorMessageForStandardMatchers() {
+
+        CookieMatcher cookieMatcher = new CookieMatcher();
+        cookieMatcher.setCookieName("DEVICE_ID");
+        cookieMatcher.setMatcher(Matchers.containsString("X"));
+
+        Map<String, Object> result = (Map<String, Object>) cookieMatcher.validateCookie(Arrays.asList(cookies));
+        assertThat((Boolean)result.get("success"), equalTo(false));
+        assertThat(result.get("errorMessage").toString(), equalTo("Expected cookie \"DEVICE_ID\" was not a string containing \"X\", was \"123\".\n"));
+    }
+
+    @Test
+    public void shouldPrintValidErrorMessageForCustomMatcher() {
+
+        CookieMatcher cookieMatcher = new CookieMatcher();
+        cookieMatcher.setCookieName("DEVICE_ID");
+        cookieMatcher.setMatcher(new ContainsXMatcher());
+
+        Map<String, Object> result = (Map<String, Object>) cookieMatcher.validateCookie(Arrays.asList(cookies));
+        assertThat((Boolean)result.get("success"), equalTo(false));
+        assertThat(result.get("errorMessage").toString(), equalTo("Expected cookie \"DEVICE_ID\" was not containing 'X', \"123\" not containing 'X'.\n"));
+    }
+
+    private static class ContainsXMatcher extends TypeSafeDiagnosingMatcher<String> {
+
+        @Override
+        protected boolean matchesSafely(String actual, Description mismatchDescription) {
+
+            // Not this method will be called twice due to https://github.com/hamcrest/JavaHamcrest/issues/144
+
+            if (actual.contains("X")) {
+                return true;
+            }
+
+            mismatchDescription.appendValue(actual).appendText(" not containing 'X'");
+            return false;
+        }
+
+        public void describeTo(Description description) {
+            description.appendText("containing 'X'");
+        }
+    }
+
+}
