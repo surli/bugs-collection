4b01ae03bb9d45f6a28411bf61d6c99f28fa4309
Luc Esape <luc.esape@gmail.com>
2017-05-31 10:35:01 +0200

    Bug commit from the following repository owlcs/owlapi This bug commit is a reflect of source code from: http://github.com/owlcs/owlapi/commit/bb13bbc85174d8b0c004255ee811353d619af40a.

---
+++ compatibility/src/main/java/org/semanticweb/owlapi/vocab/PrefixOWLOntologyFormat.java
@@ -0,0 +1,14 @@
+package org.semanticweb.owlapi.vocab;
+
+import java.io.Serializable;
+
+import org.semanticweb.owlapi.model.OWLOntologyFormat;
+
+/**
+ * Transition interface, for compatibility purposes.
+ *
+ * @deprecated use PrefixDocumentFormat instead
+ */
+@Deprecated
+public interface PrefixOWLOntologyFormat extends OWLOntologyFormat, Serializable {
+}
