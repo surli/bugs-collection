c23389e7f6e330d78c83918630f1512c4061ca21
Luc Esape <luc.esape@gmail.com>
2017-08-01 18:01:08 +0200

    Bug commit from the following repository dangdangdotcom/sharding-jdbc This bug commit is a reflect of source code from: http://github.com/dangdangdotcom/sharding-jdbc/commit/2c2126af9716233cb202b57f0ee6d1198aa2f164.

---
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/util/SQLUtil.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package com.dangdang.ddframe.rdb.sharding.util;
+
+import com.dangdang.ddframe.rdb.sharding.exception.SQLParserException;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.contstant.SQLType;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.dialect.mysql.lexer.MySQLKeyword;
+import com.dangdang.ddframe.rdb.sharding.parsing.lexer.Lexer;
+import com.dangdang.ddframe.rdb.sharding.parsing.lexer.analyzer.Dictionary;
+import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.Assist;
+import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.Keyword;
+import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.TokenType;
+import com.google.common.base.CharMatcher;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import static com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.Assist.END;
+import static com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.DefaultKeyword.DELETE;
+import static com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.DefaultKeyword.INSERT;
+import static com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.DefaultKeyword.SELECT;
+import static com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.DefaultKeyword.UPDATE;
+
+/**
+ * SQL工具类.
+ * 
+ * @author gaohongtao
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SQLUtil {
+    
+    /**
+     * 去掉SQL表达式的特殊字符.
+     * 
+     * @param value SQL表达式
+     * @return 去掉SQL特殊字符的表达式
+     */
+    public static String getExactlyValue(final String value) {
+        return null == value ? null : CharMatcher.anyOf("[]`'\"").removeFrom(value);
+    }
+    
+    /**
+     * 安全的调用一组可能抛出{@linkplain SQLException}的对象中的方法.
+     * 通过该方法保证后，保证每个对象中的方法均被调用一次
+     * 
+     * @param throwableSQLExceptionObjects 调用方法可能抛出异常的对象集合
+     * @param method 方法定义
+     * @param <T> 对象类型
+     * @throws SQLException 数据库访问异常会抛出
+     */
+    public static <T> void safeInvoke(final Collection<T> throwableSQLExceptionObjects, final ThrowableSQLExceptionMethod<T> method) throws SQLException {
+        SQLException current = null;
+        for (T each : throwableSQLExceptionObjects) {
+            try {
+                method.apply(each);
+            } catch (final SQLException exp) {
+                if (null == current) {
+                    current = exp;
+                } else {
+                    current.setNextException(exp);
+                    current = exp; 
+                }
+            }
+        }
+        if (null != current) {
+            throw current;
+        }
+    }
+    
+    /**
+     * 根据SQL第一个单词判断SQL类型.
+     * 
+     * @param sql SQL语句
+     * @return SQL类型
+     */
+    public static SQLType getTypeByStart(final String sql) {
+        // TODO: Use new Lexer Util, only support mysql now.
+        Lexer lexer = new Lexer(sql, new Dictionary(MySQLKeyword.values()));
+        lexer.nextToken();
+        while (true) {
+            TokenType tokenType = lexer.getCurrentToken().getType();
+            if (tokenType instanceof Keyword) {
+                if (tokenType.equals(SELECT)) {
+                    return SQLType.SELECT;
+                } else if (tokenType.equals(UPDATE)) {
+                    return SQLType.UPDATE;
+                } else if (tokenType.equals(INSERT)) {
+                    return SQLType.INSERT;
+                } else if (tokenType.equals(DELETE)) {
+                    return SQLType.DELETE;
+                }
+            }
+            if (tokenType instanceof Assist) {
+                if (tokenType.equals(END)) {
+                    throw new SQLParserException("Unsupported SQL statement: [%s]", sql);
+                }
+            }
+            lexer.nextToken();
+        }
+    }
+}
---
+++ sharding-jdbc-core/src/test/java/com/dangdang/ddframe/rdb/sharding/merger/ResultSetMergeContextTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package com.dangdang.ddframe.rdb.sharding.merger;
+
+import com.dangdang.ddframe.rdb.sharding.merger.fixture.MergerTestUtil;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.contstant.AggregationType;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.contstant.OrderType;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.GroupByContext;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.OrderByContext;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.SQLContext;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.SelectSQLContext;
+import com.google.common.base.Optional;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public final class ResultSetMergeContextTest {
+    
+    @Test
+    public void assertNewResultSetMergeContext() throws SQLException {
+        ResultSetMergeContext actual = new ResultSetMergeContext(
+                new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Arrays.asList("order_col", "group_col", "count_col", "avg_col", "sharding_gen_1", "sharding_gen_2")))), 
+                createSQLContext());
+        assertThat(actual.getSqlContext().getOrderByContexts().get(0).getColumnIndex(), is(1));
+        assertThat(actual.getSqlContext().getGroupByContexts().get(0).getColumnIndex(), is(2));
+        assertThat(actual.getSqlContext().getAggregationSelectItemContexts().get(0).getColumnIndex(), is(3));
+        assertThat(actual.getSqlContext().getAggregationSelectItemContexts().get(1).getColumnIndex(), is(4));
+        assertThat(actual.getSqlContext().getAggregationSelectItemContexts().get(1).getDerivedAggregationSelectItemContexts().get(0).getColumnIndex(), is(5));
+        assertThat(actual.getSqlContext().getAggregationSelectItemContexts().get(1).getDerivedAggregationSelectItemContexts().get(1).getColumnIndex(), is(6));
+        assertThat(actual.getCurrentOrderByKeys(), is(actual.getSqlContext().getOrderByContexts()));
+    }
+    
+    private SQLContext createSQLContext() {
+        SelectSQLContext result = new SelectSQLContext();
+        result.getOrderByContexts().add(new OrderByContext("order_col", OrderType.ASC, Optional.<String>absent()));
+        result.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "group_col", OrderType.ASC, Optional.<String>absent()));
+        result.getItemContexts().add(MergerTestUtil.createAggregationColumn(AggregationType.COUNT, "count_col", "count_col", -1));
+        result.getItemContexts().add(MergerTestUtil.createAggregationColumn(AggregationType.AVG, "avg_col", "avg_col", -1));
+        return result;
+    }
+    
+    @Test
+    public void assertIsNotNeedMemorySortForGroupByWithoutGroupBy() throws SQLException {
+        ResultSetMergeContext actual = new ResultSetMergeContext(
+                new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Collections.<String>emptyList()))), new SelectSQLContext());
+        assertFalse(actual.isNeedMemorySortForGroupBy());
+    }
+    
+    @Test
+    public void assertIsNeedMemorySortForGroupByWithGroupByAndOrderBySame() throws SQLException {
+        SQLContext sqlContext = new SelectSQLContext();
+        sqlContext.getOrderByContexts().add(new OrderByContext("col", OrderType.ASC, Optional.<String>absent()));
+        sqlContext.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "col", OrderType.ASC, Optional.<String>absent()));
+        ResultSetMergeContext actual = new ResultSetMergeContext(new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Collections.singletonList("col")))), sqlContext);
+        assertFalse(actual.isNeedMemorySortForGroupBy());
+    }
+    
+    @Test
+    public void assertIsNeedMemorySortForGroupByWithGroupByAndOrderByDifferent() throws SQLException {
+        SQLContext sqlContext = new SelectSQLContext();
+        sqlContext.getOrderByContexts().add(new OrderByContext("order_col", OrderType.ASC, Optional.<String>absent()));
+        sqlContext.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "group_col", OrderType.ASC, Optional.<String>absent()));
+        ResultSetMergeContext actual = new ResultSetMergeContext(new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Arrays.asList("order_col", "group_col")))), sqlContext);
+        assertTrue(actual.isNeedMemorySortForGroupBy());
+    }
+    
+    @Test
+    public void assertSetGroupByKeysToCurrentOrderByKeys() throws SQLException {
+        SQLContext sqlContext = new SelectSQLContext();
+        sqlContext.getOrderByContexts().add(new OrderByContext("order_col", OrderType.ASC, Optional.<String>absent()));
+        sqlContext.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "group_col", OrderType.ASC, Optional.<String>absent()));
+        ResultSetMergeContext actual = new ResultSetMergeContext(
+                new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Arrays.asList("order_col", "group_col")))), sqlContext);
+        actual.setGroupByKeysToCurrentOrderByKeys();
+        assertThat(actual.getCurrentOrderByKeys().size(), is(1));
+        assertThat(actual.getCurrentOrderByKeys().get(0).getColumnName().get(), is("group_col"));
+    }
+    
+    @Test
+    public void assertIsNotNeedMemorySortForOrderByWithoutOrderBy() throws SQLException {
+        ResultSetMergeContext actual = new ResultSetMergeContext(
+                new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Collections.<String>emptyList()))), new SelectSQLContext());
+        assertFalse(actual.isNeedMemorySortForOrderBy());
+    }
+    
+    @Test
+    public void assertIsNeedMemorySortForOrderByWithGroupByAndOrderBySame() throws SQLException {
+        SQLContext sqlContext = new SelectSQLContext();
+        sqlContext.getOrderByContexts().add(new OrderByContext("col", OrderType.ASC, Optional.<String>absent()));
+        sqlContext.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "col", OrderType.ASC, Optional.<String>absent()));
+        ResultSetMergeContext actual = new ResultSetMergeContext(new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Collections.singletonList("col")))), sqlContext);
+        assertFalse(actual.isNeedMemorySortForOrderBy());
+    }
+    
+    @Test
+    public void assertIsNeedMemorySortForOrderByWithGroupByAndOrderByDifferent() throws SQLException {
+        SQLContext sqlContext = new SelectSQLContext();
+        sqlContext.getOrderByContexts().add(new OrderByContext("order_col", OrderType.ASC, Optional.<String>absent()));
+        sqlContext.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "group_col", OrderType.ASC, Optional.<String>absent()));
+        ResultSetMergeContext actual = new ResultSetMergeContext(new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Arrays.asList("order_col", "group_col")))), sqlContext);
+        actual.setGroupByKeysToCurrentOrderByKeys();
+        assertTrue(actual.isNeedMemorySortForOrderBy());
+    }
+    
+    @Test
+    public void assertSetOrderByKeysToCurrentOrderByKeys() throws SQLException {
+        SQLContext sqlContext = new SelectSQLContext();
+        sqlContext.getOrderByContexts().add(new OrderByContext("order_col", OrderType.ASC, Optional.<String>absent()));
+        sqlContext.getGroupByContexts().add(new GroupByContext(Optional.<String>absent(), "group_col", OrderType.ASC, Optional.<String>absent()));
+        ResultSetMergeContext actual = new ResultSetMergeContext(new ShardingResultSets(Collections.singletonList(MergerTestUtil.mockResult(Arrays.asList("order_col", "group_col")))), sqlContext);
+        actual.setGroupByKeysToCurrentOrderByKeys();
+        actual.setOrderByKeysToCurrentOrderByKeys();
+        assertThat(actual.getCurrentOrderByKeys().size(), is(1));
+        assertThat(actual.getCurrentOrderByKeys().get(0).getColumnName().get(), is("order_col"));
+    }
+}
