2501917babc3f18ece6f15660c038fb75b50b195
Luc Esape <luc.esape@gmail.com>
2017-09-05 15:52:45 +0200

    Bug commit from the following repository PebbleTemplates/pebble This bug commit is a reflect of source code from: http://github.com/PebbleTemplates/pebble/commit/b78c3d3012368dce1bed5b52237e0cb22d82b024.

---
+++ src/main/java/com/mitchellbosecke/pebble/node/PositionalArgumentNode.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * This file is part of Pebble.
+ * 
+ * Copyright (c) 2014 by Mitchell Bösecke
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ ******************************************************************************/
+package com.mitchellbosecke.pebble.node;
+
+import com.mitchellbosecke.pebble.extension.NodeVisitor;
+import com.mitchellbosecke.pebble.node.expression.Expression;
+
+public class PositionalArgumentNode implements Node {
+
+    private final Expression<?> value;
+
+    public PositionalArgumentNode(Expression<?> value) {
+        this.value = value;
+    }
+
+    @Override
+    public void accept(NodeVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    public Expression<?> getValueExpression() {
+        return value;
+    }
+
+}
